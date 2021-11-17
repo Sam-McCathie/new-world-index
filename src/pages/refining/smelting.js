efb86d9014c797dd0dd995864cbcb0404f174f8e
sam-mccathie <sammccathie2@gmail.com>
2021-11-17 18:17:12 +1300

    Update smelting item list from DB

--- src/pages/refining/smelting.js
+++ src/pages/refining/smelting.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react";
 
 import "../../styles/smelting.scss";
-// import {Refining} from "../../components/refining";
-// import {Metals, PreciousMetals, MaterialConversion} from "../../data/smelting";
-import {items} from "../../data/items";
+import Axios from "axios";
+
 import {SmeltingItem} from "../../components/refining/smelting/SmeltingItem";
 
 export const Smelting = () => {
+  const [allItems, setAllItems] = useState();
+
   const [charcoal, setCharcoal] = useState();
   const [greenwood, setGreenwood] = useState();
   const [sandFlux, setSandFlux] = useState();
@@ -36,74 +37,79 @@ export const Smelting = () => {
   const [platinumIngot, setPlatinumIngot] = useState();
   const [platinumOre, setPlatinumOre] = useState();
 
-  useEffect(() => {
-    for (let i = 0; i <= items.length - 1; i++) {
-      if (items[i].item === "Charcoal") {
-        setCharcoal(items[i].value);
-      } else if (items[i].item === "Greenwood") {
-        setGreenwood(items[i].value);
-      } else if (items[i].item === "Sand Flux") {
-        setSandFlux(items[i].value);
-      } else if (items[i].item === "Obsidian Flux") {
-        setObsidianFlux(items[i].value);
-      } else if (items[i].item === "Iron Ingot") {
-        setIronIngot(items[i].value);
-      } else if (items[i].item === "Iron Ore") {
-        setIronOre(items[i].value);
-      } else if (items[i].item === "Steel Ingot") {
-        setSteelIngot(items[i].value);
-      } else if (items[i].item === "Starmetal Ingot") {
-        setStarmetalIngot(items[i].value);
-      } else if (items[i].item === "Starmetal Ore") {
-        setStarmetalOre(items[i].value);
-      } else if (items[i].item === "Asmodeum Ingot") {
-        setAsmodeumIngot(items[i].value);
-      } else if (items[i].item === "Orichalecum Ingot") {
-        setOrichalecumIngot(items[i].value);
-      } else if (items[i].item === "Orichalecum Ore") {
-        setOrichalecumOre(items[i].value);
-      } else if (items[i].item === "Tolvium") {
-        setTolvium(items[i].value);
-      } else if (items[i].item === "Cinnibar") {
-        setCinnibar(items[i].value);
-      } else if (items[i].item === "Voidbent Ingot") {
-        setVoidbentIngot(items[i].value);
-      } else if (items[i].item === "Void Ore") {
-        setVoidOre(items[i].value);
-      } else if (items[i].item === "Void Essence") {
-        setVoidEssence(items[i].value);
-      } else if (items[i].item === "Energy Core") {
-        setEnergryCore(items[i].value);
-      } else if (items[i].item === "Silver Ingot") {
-        setSilverIngot(items[i].value);
-      } else if (items[i].item === "Silver Ore") {
-        setSilverOre(items[i].value);
-      } else if (items[i].item === "Gold Ingot") {
-        setGoldIngot(items[i].value);
-      } else if (items[i].item === "Gold Ore") {
-        setGoldOre(items[i].value);
-      } else if (items[i].item === "Platinum Ingot") {
-        setPlatinumIngot(items[i].value);
-      } else if (items[i].item === "Platinum Ore") {
-        setPlatinumOre(items[i].value);
-      }
-    }
-  }, []);
+  // console.log(greenwood);
 
-  console.log(ironIngot);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/all-items").then((response) => {
+      let items = response.data;
+      setAllItems(items);
+      for (let i = 0; i <= items.length - 1; i++) {
+        if (items[i].item === "Charcoal") {
+          setCharcoal(items[i].value);
+        } else if (items[i].item === "Green Wood") {
+          setGreenwood(items[i].value);
+        } else if (items[i].item === "Sand Flux") {
+          setSandFlux(items[i].value);
+        } else if (items[i].item === "Obsidian Flux") {
+          setObsidianFlux(items[i].value);
+        } else if (items[i].item === "Iron Ingot") {
+          setIronIngot(items[i].value);
+        } else if (items[i].item === "Iron Ore") {
+          setIronOre(items[i].value);
+        } else if (items[i].item === "Steel Ingot") {
+          setSteelIngot(items[i].value);
+        } else if (items[i].item === "Starmetal Ingot") {
+          setStarmetalIngot(items[i].value);
+        } else if (items[i].item === "Starmetal Ore") {
+          setStarmetalOre(items[i].value);
+        } else if (items[i].item === "Asmodeum Ingot") {
+          setAsmodeumIngot(items[i].value);
+        } else if (items[i].item === "Orichalecum Ingot") {
+          setOrichalecumIngot(items[i].value);
+        } else if (items[i].item === "Orichalecum Ore") {
+          setOrichalecumOre(items[i].value);
+        } else if (items[i].item === "Tolvium") {
+          setTolvium(items[i].value);
+        } else if (items[i].item === "Cinnibar") {
+          setCinnibar(items[i].value);
+        } else if (items[i].item === "Voidbent Ingot") {
+          setVoidbentIngot(items[i].value);
+        } else if (items[i].item === "Void Ore") {
+          setVoidOre(items[i].value);
+        } else if (items[i].item === "Void Essence") {
+          setVoidEssence(items[i].value);
+        } else if (items[i].item === "Energy Core") {
+          setEnergryCore(items[i].value);
+        } else if (items[i].item === "Silver Ingot") {
+          setSilverIngot(items[i].value);
+        } else if (items[i].item === "Silver Ore") {
+          setSilverOre(items[i].value);
+        } else if (items[i].item === "Gold Ingot") {
+          setGoldIngot(items[i].value);
+        } else if (items[i].item === "Gold Ore") {
+          setGoldOre(items[i].value);
+        } else if (items[i].item === "Platinum Ingot") {
+          setPlatinumIngot(items[i].value);
+        } else if (items[i].item === "Platinum Ore") {
+          setPlatinumOre(items[i].value);
+        }
+      }
+    });
+  }, []);
 
   return (
     <div className="smelting">
       <div className="smelting__layout">
         <div className="smelting__item__container">
           <h1>Item List</h1>
-          <h3>Charcoal & Flux</h3>
+          <h3 className="smelting__subheading">Charcoal & Flux</h3>
           <div className="smelting__layout">
             <div className="smelting__item__header__item">Item</div>
             <div className="smelting__item__header__value">Value</div>
             <div className="smelting__item__header__level">Level</div>
           </div>
-          <h3>Metals</h3>
+          <SmeltingItem item="Charcoal" value={charcoal} level="0" />
+          <h3 className="smelting__subheading">Metals</h3>
           <div className="smelting__layout">
             <div className="smelting__item__header__item">Item</div>
             <div className="smelting__item__header__value">Value</div>
@@ -111,10 +117,25 @@ export const Smelting = () => {
           </div>
           <SmeltingItem item="Iron Ingot" value={ironIngot} level="0" />
           <SmeltingItem item="Steel Ingot" value={steelIngot} level="0" />
-          {/* <SmeltingItem item="Iron Ingot" value={ironIngot} level="0" />
-          <SmeltingItem item="Iron Ingot" value={ironIngot} level="0" />
-          <SmeltingItem item="Iron Ingot" value={ironIngot} level="0" />
-          <SmeltingItem item="Iron Ingot" value={ironIngot} level="0" /> */}
+          <SmeltingItem
+            item="Starmetal Ingot"
+            value={starmetalIngot}
+            level="0"
+          />
+          <SmeltingItem
+            item="Orichalecum Ingot"
+            value={orichalecumIngot}
+            level="0"
+          />
+          <h3 className="smelting__subheading">Precious Metals</h3>
+          <div className="smelting__layout">
+            <div className="smelting__item__header__item">Item</div>
+            <div className="smelting__item__header__value">Value</div>
+            <div className="smelting__item__header__level">Level</div>
+          </div>
+          <SmeltingItem item="Silver Ingot" value={silverIngot} level="0" />
+          <SmeltingItem item="Gold Ingot" value={goldIngot} level="0" />
+          <SmeltingItem item="Platinum Ingot" value={platinumIngot} level="0" />
         </div>
         <div className="smelting__recipe">
           <h3>Recipe</h3>
